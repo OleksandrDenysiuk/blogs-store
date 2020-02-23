3723b246f6d34fae0234f94c19937fbde4d05a0e
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-23 16:43:49 +0100

    Initial commit

---
+++ src/main/java/com/portfolio/blogsstore/BlogsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.blogsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsStoreApplication.class, args);
+    }
+
+}

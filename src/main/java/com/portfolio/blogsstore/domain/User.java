77437acab1a688b87495f33d05e94bf5ea0fdd20
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-28 21:59:24 +0100

    Implemented UserDetails

--- src/main/java/com/portfolio/blogsstore/domain/User.java
+++ src/main/java/com/portfolio/blogsstore/domain/User.java
@@ -2,26 +2,36 @@ package com.portfolio.blogsstore.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
 @Setter
-@ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "User")
-public class User{
+public class User implements UserDetails {
 
     @Id
-    private Long id;
+    private String id;
 
     private String username;
 
     private String password;
+
+    @Transient
+    private String passwordConfirm;
 
     private String firstName;
     private String lastName;
@@ -30,5 +40,68 @@ public class User{
 
     private String address;
 
-    private Set<Role> roles;
+    private Set<Role> roles = new HashSet<>();
+
+    @DBRef
+    private Set<Article> articles = new HashSet<>();
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(username, user.username) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(address, user.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, firstName, lastName, email, address);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }

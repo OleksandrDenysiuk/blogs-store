2b5a78c31a800c966916142792eb5bf2b149a684
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-02-28 22:04:26 +0100

    Added UserValidator

---
+++ src/main/java/com/portfolio/blogsstore/validator/UserValidator.java
@@ -0,0 +1,44 @@
+package com.portfolio.blogsstore.validator;
+
+import com.portfolio.blogsstore.domain.User;
+import com.portfolio.blogsstore.service.UserService;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+
+@Component
+public class UserValidator implements Validator {
+
+    private final UserService userService;
+
+    public UserValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "EmptyOrWhiteSpace");
+        if (user.getUsername().length() < 6 || user.getUsername().length() > 32) {
+            errors.rejectValue("username", "Size.username");
+        }
+        if (userService.findByUsername(user.getUsername()) != null) {
+            errors.rejectValue("username", "Duplicate.username");
+        }
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "EmptyOrWhiteSpace");
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+            errors.rejectValue("password", "Size.password");
+        }
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwordConfirm", "EmptyOrWhiteSpace");
+        if (!user.getPassword().equals(user.getPasswordConfirm())) {
+            errors.rejectValue("passwordConfirm", "ConfirmPassword");
+        }
+    }
+}

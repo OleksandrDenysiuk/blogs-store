a0509a5d2d3334217bd5a04ca7d4045a568b6ee1
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-01 23:25:45 +0100

    Added render image for user profile

--- src/main/java/com/portfolio/blogsstore/controller/ImageController.java
+++ src/main/java/com/portfolio/blogsstore/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.portfolio.blogsstore.controller;
 
 
 import com.portfolio.blogsstore.domain.Article;
+import com.portfolio.blogsstore.domain.User;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +15,16 @@ import java.io.InputStream;
 
 @Controller
 public class ImageController {
+
     @GetMapping("article/{id}/image")
-    public void renderImageRecipeFromDB(@PathVariable("id") Article article, HttpServletResponse response) throws IOException {
+    public void renderImageArticleFromDB(@PathVariable("id") Article article, HttpServletResponse response) throws IOException {
 
         renderImage(response, article.getImage());
     }
 
-
-    @GetMapping("step/{id}/image")
-    public void renderImageStepFromDB(@PathVariable("id") Article article, HttpServletResponse response) throws IOException {
-        renderImage(response, article.getImage());
+    @GetMapping("user/{id}/image")
+    public void renderImageProfileFromDB(@PathVariable("id") User user, HttpServletResponse response) throws IOException {
+        renderImage(response, user.getImage());
     }
 
     private void renderImage(HttpServletResponse response, Byte[] image) throws IOException {

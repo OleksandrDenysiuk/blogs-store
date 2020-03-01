1a877a480c8345a41000eea01338d232b6141379
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-01 23:30:15 +0100

    Added saveImageFile method for user

--- src/main/java/com/portfolio/blogsstore/service/ImageService.java
+++ src/main/java/com/portfolio/blogsstore/service/ImageService.java
@@ -1,8 +1,10 @@
 package com.portfolio.blogsstore.service;
 
 import com.portfolio.blogsstore.domain.Article;
+import com.portfolio.blogsstore.domain.User;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
     void saveImageFile(Article article, MultipartFile file);
+    void saveImageFile(User user, MultipartFile file);
 }

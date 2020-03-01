cde223ccbe51a703737bbc863b34065b26f9d76b
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-03-01 23:32:47 +0100

    Added ArticleValidator

---
+++ src/main/java/com/portfolio/blogsstore/validator/ArticleValidator.java
@@ -0,0 +1,28 @@
+package com.portfolio.blogsstore.validator;
+
+import com.portfolio.blogsstore.domain.Article;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class ArticleValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Article.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Article article = (Article) target;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "EmptyOrWhiteSpace");
+        if (article.getTitle().length() < 4 || article.getTitle().length() > 32) {
+            errors.rejectValue("title", "Size.title");
+        }
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "content", "EmptyOrWhiteSpace");
+        if (article.getContent().length() < 12) {
+            errors.rejectValue("content", "Size.content");
+        }
+    }
+}
